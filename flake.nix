16107d7038ef97931202dc5ab133595b8e5b0f6d
Simon Scatton <sscatton@nvidia.com>
2026-07-10 16:04:29 +0200

    chore(nix): add bazel dev shell flake
    Nix supplies bazel itself (nix-built, so it runs on NixOS without a
    loader shim); Bazel supplies all build toolchains. Replaces the old
    mise toolchain layer and the previous rust/z3 dev shell.
    
    Signed-off-by: Simon Scatton <sscatton@nvidia.com>

--- flake.nix
+++ flake.nix
@@ -1,61 +1,25 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-
 {
-  description = "OpenShell development environment";
+  description = "OpenShell development shell";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      flake-utils,
-      nixpkgs,
-      rust-overlay,
-      treefmt-nix,
-      ...
-    }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (
-      system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        treefmtEval = treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
-          programs.nixfmt.enable = true;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            # Required to find packages
-            pkg-config
-            # Required for bindgen generation.
-            llvmPackages.libclang
-            # system dependency for openshell-prover
-            z3
+          packages = [
+            # Bazel provides all build toolchains (rustc, protoc, cc) itself;
+            # nix only has to supply bazel.
+            pkgs.bazel_9
           ];
-
-          env = {
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          };
         };
-
-        formatter = treefmtEval.config.build.wrapper;
-      }
-    );
+      });
 }
